3fbf25775d720f716d8a35849147c0278237dc94
jinto josan <jintojosan@gmail.com>
2024-10-02 22:43:25 +0530

    Updated button with material

--- Mindara/src/app/library/tile-container/tile-container.component.ts
+++ Mindara/src/app/library/tile-container/tile-container.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule} from '@angular/material/button'
 export type Tile = { label: string, link: string };
 
 @Component({
   selector: 'app-tile-container',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,MatButtonModule],
   templateUrl: './tile-container.component.html',
   styleUrl: './tile-container.component.scss',
 })
