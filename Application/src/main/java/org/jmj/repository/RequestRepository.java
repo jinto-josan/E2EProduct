9d7cd829e803e67c0901d080dc9722381b94561f
jinto josan <jintojosan@gmail.com>
2024-10-19 18:17:49 +0530

    Clearing native query

--- Application/src/main/java/org/jmj/repository/RequestRepository.java
+++ Application/src/main/java/org/jmj/repository/RequestRepository.java
@@ -16,9 +16,6 @@ public interface RequestRepository extends JpaRepository<Request, RequestId> {
 
     //Todo: Need to use projection to get only the path and method
     @Query("SELECT r.id FROM Request r WHERE r.subSystem.name = :subSystemName")
-//    @Query(value = "SELECT r.method, r.path FROM Request r WHERE r.sub_system_id = ?1",
-//            countQuery = "SELECT count(*) FROM Request r WHERE r.sub_system_id = ?1",
-//            nativeQuery = true)
     Page<RequestId> findBySubSystemName(String subSystemName, Pageable pageable);
     //Name is used because not required to load requests.json in subsystem eagerly
     @Query("SELECT DISTINCT r.id.path FROM Request r WHERE r.subSystem.name = :subSystemName")
